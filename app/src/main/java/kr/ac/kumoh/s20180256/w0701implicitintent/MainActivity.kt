848a970bd8c2a97377efe2dfe512930366ce4fc4
kiminchan <kiminchan12341@gmail.com>
2022-10-19 14:34:42 +0900

    map, geo intent

--- app/src/main/java/kr/ac/kumoh/s20180256/w0701implicitintent/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180256/w0701implicitintent/MainActivity.kt
@@ -14,7 +14,20 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
         binding.btnHomepage.setOnClickListener {
-            var uri = Uri.parse("http://bwoh.githun.io")
+            var uri = Uri.parse("https://www.instagram.com/dlwlrma/")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        binding.btnMap.setOnClickListener {
+            //var uri = Uri.parse("geo:36.145014,128.393047?z=17")
+            var uri = Uri.parse("geo:0,0?z=17&q=경북 구미시 대학로 3길 38")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        binding.btnTelephone.setOnClickListener {
+            var uri = Uri.parse("sms:010-4614-7221")
             val intent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(intent)
         }
